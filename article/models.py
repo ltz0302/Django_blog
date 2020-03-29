b8f740f380aaa882efdbb6e9aad14448b6cd708f
ltz0302 <45015812+ltz0302@users.noreply.github.com>
2020-03-29 18:21:49 +0800

    first version

---
+++ article/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+class ArticlePost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    # 文章更新时间。参数 auto_now=True 指定每次数据更新时自动写入当前时间
+    updated = models.DateTimeField(auto_now=True)
+    total_views = models.PositiveIntegerField(default=0)
+    # 文章标签
+    tags = TaggableManager(blank=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        # ordering 指定模型返回的数据的排列顺序
+        # '-created' 表明数据应该以倒序排列
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article:article_detail', args=[self.id])
---
+++ article/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render, redirect
+from .models import ArticlePost
+from django.http import HttpResponse
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
+from comment.models import Comment
+from comment.forms import CommentForm
+import markdown
+# Create your views here.
+
+def article_list(request):
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    tag = request.GET.get('tag')
+
+    article_list = ArticlePost.objects.all()
+
+    # 搜索查询集
+    if search:
+        article_list = article_list.filter(
+            Q(title__icontains=search) |
+            Q(body__icontains=search)
+        )
+    else:
+        search = ''
+
+
+    # 标签查询集
+    if tag and tag != 'None':
+        article_list = article_list.filter(tags__name__in=[tag])
+
+    # 查询集排序
+    if order == 'total_views':
+        article_list = article_list.order_by('-total_views')
+
+    paginator = Paginator(article_list, 3)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    # 需要传递给模板（templates）的对象
+    context = {
+        'articles': articles,
+        'order': order,
+        'search': search,
+        'tag': tag,
+    }
+
+    return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    comments = Comment.objects.filter(article=id)
+
+    md = markdown.Markdown(
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ]
+    )
+    article.body = md.convert(article.body)
+
+    # 浏览量 +1
+    article.total_views += 1
+    article.save(update_fields=['total_views'])
+
+    comment_form = CommentForm()
+    context = {'article': article, 'toc': md.toc, 'comments': comments, 'comment_form': comment_form,}
+    return render(request, 'article/detail.html', context)
+
+@login_required(login_url='/userprofile/login/')
+def article_create(request):
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=request.user.id)
+            new_article.save()
+            article_post_form.save_m2m()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
+
+@login_required(login_url='/userprofile/login/')
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        # 过滤非作者的用户
+        if request.user != article.author:
+            return HttpResponse("抱歉，你无权修改这篇文章。")
+        article.delete()
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("仅允许post请求")
+
+@login_required(login_url='/userprofile/login/')
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    # 过滤非作者的用户
+    if request.user != article.author:
+        return HttpResponse("抱歉，你无权修改这篇文章。")
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            # 保存新写入的 title、body 数据并保存
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            # 完成后返回到修改后的文章中。需传入文章的 id 值
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        context = {'article': article}
+        return render(request, 'article/update.html', context)
+
+
+def increase_likes(request, id):
+    if request.method == "POST":
+        article = ArticlePost.objects.get(id=id)
+        article.likes += 1
+        article.save()
+        return HttpResponse('success')
